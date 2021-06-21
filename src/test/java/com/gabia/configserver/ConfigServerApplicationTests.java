de8abd4b358b9575152b3699f6663ba11e5e9847
Root(김명석) <mskim@gabia.com>
2021-06-21 15:38:38 +0900

    init: Config server 프로젝트 세팅

---
+++ src/test/java/com/gabia/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
